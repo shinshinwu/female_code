89a72a02404bb580940aa06c01426b617e71f3f5
Anna Wu <shinshinwu@gmail.com>
2016-01-12 22:49:13 -0800

    updated style and moved over pie chart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
   def new
+    @companies          = Company.all
+    # pie chart
+    total_num_females   = @companies.sum(:num_female_eng)
+    total_num_engineers = @companies.sum(:num_eng)
+    total_num_males     = total_num_engineers - total_num_females
+    female_percentage   =  total_num_females / total_num_engineers.to_f
+    gon.female_percentage    = female_percentage
+    gon.female_engineers_num = total_num_females.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+    gon.male_percentage      = 1 - female_percentage
+    gon.male_engineers_num   = total_num_males.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 end
