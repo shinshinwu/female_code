9c9f74a78abd14791cb0cf16acc9d57fa6012939
Anna Wu <shinshinwu@gmail.com>
2016-01-18 22:44:20 -0800

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users, only: [:show, :update] do
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
