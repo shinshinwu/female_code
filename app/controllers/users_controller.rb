acc8990e40dff9b996b05476790fab0e3a32639c
Anna Wu <shinshinwu@gmail.com>
2016-01-27 16:25:40 -0800

    update graph data structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
 
   def new
-    @companies               = Company.all
-    # TODO: need to update this to adapt the new data structure
+    @company_stats           = CompanyStaffStat.approved
     # pie chart
-    # total_num_females        = @companies.sum(:num_female_eng)
-    # total_num_engineers      = @companies.sum(:num_eng)
-    # total_num_males          = total_num_engineers - total_num_females
-    # female_percentage        =  total_num_females / total_num_engineers.to_f
-    # gon.female_percentage    = female_percentage
-    # gon.female_engineers_num = total_num_females.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-    # gon.male_percentage      = 1 - female_percentage
-    # gon.male_engineers_num   = total_num_males.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+    total_num_females        = @company_stats.sum(:num_female_eng)
+    total_num_engineers      = @company_stats.sum(:num_eng)
+    total_num_males          = total_num_engineers - total_num_females
+    female_percentage        =  total_num_females / total_num_engineers.to_f
+    gon.female_percentage    = female_percentage
+    gon.female_engineers_num = total_num_females.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+    gon.male_percentage      = 1 - female_percentage
+    gon.male_engineers_num   = total_num_males.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 
   def show
--- app/models/company_staff_stat.rb
+++ app/models/company_staff_stat.rb
@@ -1,4 +1,10 @@
 class CompanyStaffStat < ActiveRecord::Base
   belongs_to :company
   belongs_to :user
+
+  scope :approved, -> { where(is_approved: true) }
+
+  def is_approved?
+    is_approved
+  end
 end
